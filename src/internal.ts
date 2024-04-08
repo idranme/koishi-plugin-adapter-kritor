23dc8cdd74f7f6e8df78934d330f8a9a6926b002
idanran <96647698+idanran@users.noreply.github.com>
2024-04-08 14:15:41 +0000

    fix: sendMessage

--- src/internal.ts
+++ src/internal.ts
@@ -1,25 +1,28 @@
-import { init, sendMessage } from './api'
+import { init } from './api'
 import { Contact } from './generated/kritor/common/Contact'
+import { Element } from './generated/kritor/common/Element'
+import { SendMessageResponse__Output } from './generated/kritor/message/SendMessageResponse'
 
 export class Internal {
     client: ReturnType<typeof init>
 
     /**
      * 发送消息
-     * @param channelId
-     * @param content
-     * @returns
      */
-    async sendMessage(channelId, content) {
-        let contact: Contact = { scene: 'GROUP', peer: channelId }
-        if (channelId.startsWith('private:')) {
-            contact.scene = "FRIEND"
-            contact.peer = channelId.replace('private:', '')
-        }
-        let res: { messageId: string, messageTime: number }
-        sendMessage(this.client, contact, content, 3, (messageId, messageTime) => {
-            res = { messageId, messageTime }
+    sendMessage(channelId: string, elements: Element[]) {
+        return new Promise<SendMessageResponse__Output>((resolve, reject) => {
+            let contact: Contact = { scene: 'GROUP', peer: channelId }
+            if (channelId.startsWith('private:')) {
+                contact.scene = "FRIEND"
+                contact.peer = channelId.replace('private:', '')
+            }
+            const { messageClient } = this.client
+            messageClient.sendMessage({ contact, elements, retryCount: 3 }, (err, response) => {
+                if (err) {
+                    throw err
+                }
+                resolve(response)
+            })
         })
-        return res
     }
 }
