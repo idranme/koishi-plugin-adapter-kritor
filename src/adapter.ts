11a6794ffc836f91a64749b731b6425cb101dd57
idanran <idanran@outlook.com>
2024-04-16 19:16:46 +0800

    optimize

--- src/adapter.ts
+++ src/adapter.ts
@@ -1,4 +1,4 @@
-import { Adapter, Context, Universal } from 'koishi'
+import { Adapter, Context, Universal, Logger } from 'koishi'
 import { KritorBot } from './bot'
 import { createSession, decodeLoginUser } from './utils'
 import * as grpc from '@grpc/grpc-js'
@@ -11,15 +11,22 @@ type SubtypeConstructor<Constructor extends new (...args: any) => any, Subtype> 
 
 export class KritorAdapter<C extends Context = Context, B extends KritorBot<C> = KritorBot<C>> extends Adapter<C, B> {
     client: ReturnType<typeof this.init>
+    private logger: Logger
 
     constructor(ctx: C, private bot: B) {
         super(ctx)
         bot.adapter = this
+        this.logger = bot.logger
     }
 
     async connect() {
         try {
             this.client = this.init(this.bot.config.address)
+            this.ctx.on('dispose', () => {
+                for (const item of Object.values(this.client)) {
+                    grpc.closeClient(item)
+                }
+            })
             const account = await this.bot.internal.getCurrentAccount()
             const { accountUin } = account
             if (accountUin.toString() !== this.bot.config.selfId) {
@@ -32,11 +39,11 @@ export class KritorAdapter<C extends Context = Context, B extends KritorBot<C> =
                 user: decodeLoginUser(account),
                 status: Universal.Status.ONLINE
             })
-            this.bot.logger.info('connect to server: %c', this.bot.config.address)
+            this.logger.info('connect to server: %c', this.bot.config.address)
         }
         catch (err) {
             this.bot.offline(err)
-            if (err.message) this.bot.logger.warn(err.message)
+            if (err.message) this.logger.warn(err.message)
         }
     }
 
@@ -46,7 +53,7 @@ export class KritorAdapter<C extends Context = Context, B extends KritorBot<C> =
 
     private async onData(input: Kritor.EventStructure__Output, type: Kritor.EventType) {
         // debug
-        this.bot.logger.info(input)
+        this.logger.info(input)
         const session = await createSession(this.bot, input)
         if (session) this.bot.dispatch(session)
     }
@@ -56,13 +63,13 @@ export class KritorAdapter<C extends Context = Context, B extends KritorBot<C> =
     }
 
     private onError(err: Error, type: Kritor.EventType) {
-        this.bot.logger.debug(err)
+        this.logger.debug(err)
     }
 
     private registerActiveListener(type: Kritor.EventType) {
         const { eventClient } = this.client
         const eventStream = eventClient.RegisterActiveListener({ type })
-        eventStream.on('data', (chunk) => this.onData(chunk, type))
+        eventStream.on('data', async (chunk) => await this.onData(chunk, type))
         eventStream.on('end', () => this.onEnd(type))
         eventStream.on('error', (err) => this.onError(err, type))
         this.ctx.on('dispose', () => {
@@ -72,35 +79,36 @@ export class KritorAdapter<C extends Context = Context, B extends KritorBot<C> =
 
     private init(address: string, timeout = 5000) {
         // Forked from https://github.com/KarinJS/kritor-ts/blob/2f7cf14012ce72e8d92e6a0426acbf76be270ea0/src/api.ts#L45
+
         const authenticationProtoGrpcType = this.getProtoGrpcType('auth/authentication.proto', [__dirname + '/kritor/protos']) as Kritor.AuthenticationProtoGrpcType
         const coreProtoGrpcType = this.getProtoGrpcType('core/core.proto', [__dirname + '/kritor/protos']) as Kritor.CoreProtoGrpcType
-        const customizationProtoGrpcType = this.getProtoGrpcType('developer/customization.proto', [__dirname + '/kritor/protos']) as Kritor.CustomizationProtoGrpcType
-        const developerProtoGrpcType = this.getProtoGrpcType('developer/developer.proto', [__dirname + '/kritor/protos']) as Kritor.DeveloperProtoGrpcType
         const eventProtoGrpcType = this.getProtoGrpcType('event/event.proto', [__dirname + '/kritor/protos']) as Kritor.EventProtoGrpcType
         const friendProtoGrpcType = this.getProtoGrpcType('friend/friend.proto', [__dirname + '/kritor/protos']) as Kritor.FriendProtoGrpcType
         const groupProtoGrpcType = this.getProtoGrpcType('group/group.proto', [__dirname + '/kritor/protos']) as Kritor.GroupProtoGrpcType
         const groupFileProtoGrpcType = this.getProtoGrpcType('file/group_file.proto', [__dirname + '/kritor/protos']) as Kritor.GroupFileProtoGrpcType
-        const guildProtoGrpcType = this.getProtoGrpcType('guild/guild.proto', [__dirname + '/kritor/protos']) as Kritor.GuildProtoGrpcType
         const messageProtoGrpcType = this.getProtoGrpcType('message/message.proto', [__dirname + '/kritor/protos']) as Kritor.MessageProtoGrpcType
-        const qsignProtoGrpcType = this.getProtoGrpcType('developer/qsign.proto', [__dirname + '/kritor/protos']) as Kritor.QsignProtoGrpcType
-        const reverseProtoGrpcType = this.getProtoGrpcType('reverse/reverse.proto', [__dirname + '/kritor/protos']) as Kritor.ReverseProtoGrpcType
-        const webProtoGrpcType = this.getProtoGrpcType('web/web.proto', [__dirname + '/kritor/protos']) as Kritor.WebProtoGrpcType
+        //const customizationProtoGrpcType = this.getProtoGrpcType('developer/customization.proto', [__dirname + '/kritor/protos']) as Kritor.CustomizationProtoGrpcType
+        //const developerProtoGrpcType = this.getProtoGrpcType('developer/developer.proto', [__dirname + '/kritor/protos']) as Kritor.DeveloperProtoGrpcType
+        //const guildProtoGrpcType = this.getProtoGrpcType('guild/guild.proto', [__dirname + '/kritor/protos']) as Kritor.GuildProtoGrpcType
+        //const qsignProtoGrpcType = this.getProtoGrpcType('developer/qsign.proto', [__dirname + '/kritor/protos']) as Kritor.QsignProtoGrpcType
+        //const reverseProtoGrpcType = this.getProtoGrpcType('reverse/reverse.proto', [__dirname + '/kritor/protos']) as Kritor.ReverseProtoGrpcType
+        //const webProtoGrpcType = this.getProtoGrpcType('web/web.proto', [__dirname + '/kritor/protos']) as Kritor.WebProtoGrpcType
 
         const credential = grpc.credentials.createInsecure()
 
         const authenticationClient = this.getClient(authenticationProtoGrpcType.kritor.authentication.AuthenticationService, address, credential)
-        const reverseClient = this.getClient(reverseProtoGrpcType.kritor.reverse.ReverseService, address, credential)
         const coreClient = this.getClient(coreProtoGrpcType.kritor.core.CoreService, address, credential)
-        const customizationClient = this.getClient(customizationProtoGrpcType.kritor.customization.CustomizationService, address, credential)
-        const developerClient = this.getClient(developerProtoGrpcType.kritor.developer.DeveloperService, address, credential)
-        const qsignClient = this.getClient(qsignProtoGrpcType.kritor.developer.QsignService, address, credential)
         const eventClient = this.getClient(eventProtoGrpcType.kritor.event.EventService, address, credential)
         const groupFileClient = this.getClient(groupFileProtoGrpcType.kritor.file.GroupFileService, address, credential)
         const friendClient = this.getClient(friendProtoGrpcType.kritor.friend.FriendService, address, credential)
         const groupClient = this.getClient(groupProtoGrpcType.kritor.group.GroupService, address, credential)
-        const guildClient = this.getClient(guildProtoGrpcType.kritor.guild.GuildService, address, credential)
         const messageClient = this.getClient(messageProtoGrpcType.kritor.message.MessageService, address, credential)
-        const webClient = this.getClient(webProtoGrpcType.kritor.web.WebService, address, credential)
+        //const reverseClient = this.getClient(reverseProtoGrpcType.kritor.reverse.ReverseService, address, credential)
+        //const customizationClient = this.getClient(customizationProtoGrpcType.kritor.customization.CustomizationService, address, credential)
+        //const developerClient = this.getClient(developerProtoGrpcType.kritor.developer.DeveloperService, address, credential)
+        //const qsignClient = this.getClient(qsignProtoGrpcType.kritor.developer.QsignService, address, credential)
+        //const guildClient = this.getClient(guildProtoGrpcType.kritor.guild.GuildService, address, credential)
+        //const webClient = this.getClient(webProtoGrpcType.kritor.web.WebService, address, credential)
 
         const deadline = new Date()
         deadline.setSeconds(deadline.getSeconds() + timeout)
@@ -112,18 +120,12 @@ export class KritorAdapter<C extends Context = Context, B extends KritorBot<C> =
 
         return {
             authenticationClient,
-            reverseClient,
             coreClient,
-            customizationClient,
-            developerClient,
-            qsignClient,
             eventClient,
             groupFileClient,
             friendClient,
             groupClient,
-            guildClient,
-            messageClient,
-            webClient,
+            messageClient
         }
     }
 
