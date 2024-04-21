2f384529884723bec2aa6c077592e48b451fc4c1
idranme <96647698+idranme@users.noreply.github.com>
2024-04-21 19:10:18 +0800

    optimize

--- src/internal.ts
+++ src/internal.ts
@@ -7,89 +7,95 @@ export class Internal {
 
     sendMessage(contact: Kritor.Contact, elements: Kritor.Element[]) {
         const { messageClient } = this.bot.adapter.client
-        const call = promisify(messageClient.sendMessage)
+        const call = promisify(messageClient.sendMessage).bind(messageClient)
         return call({ contact, elements, retryCount: 3 })
     }
 
     getCurrentAccount() {
         const { coreClient } = this.bot.adapter.client
-        const call = promisify(coreClient.getCurrentAccount)
+        const call = promisify(coreClient.getCurrentAccount).bind(coreClient)
         return call({})
     }
 
     getUidByUin(targetUins: (string | number)[]) {
         const { friendClient } = this.bot.adapter.client
-        const call = promisify(friendClient.getUidByUin)
+        const call = promisify(friendClient.getUidByUin).bind(friendClient)
         return call({ targetUins })
     }
 
     recallMessage(contact: Kritor.Contact, messageId: string) {
         const { messageClient } = this.bot.adapter.client
-        const call = promisify(messageClient.recallMessage)
+        const call = promisify(messageClient.recallMessage).bind(messageClient)
         return call({ contact, messageId })
     }
 
     getGroupList(refresh?: boolean) {
         const { groupClient } = this.bot.adapter.client
-        const call = promisify(groupClient.getGroupList)
+        const call = promisify(groupClient.getGroupList).bind(groupClient)
         return call({ refresh })
     }
 
     getGroupInfo(groupId: number | string) {
         const { groupClient } = this.bot.adapter.client
-        const call = promisify(groupClient.getGroupInfo)
+        const call = promisify(groupClient.getGroupInfo).bind(groupClient)
         return call({ groupId })
     }
 
     // sham 报错 UNKNOWN
     getGroupMemberInfo(groupId: number | string, targetUin: number | string, refresh?: boolean) {
         const { groupClient } = this.bot.adapter.client
-        const call = promisify(groupClient.getGroupMemberInfo)
+        const call = promisify(groupClient.getGroupMemberInfo).bind(groupClient)
         return call({ groupId, targetUin, target: 'targetUin', refresh })
     }
 
     // sham 报错 UNKNOWN
     getGroupMemberList(groupId: number | string, refresh?: boolean) {
         const { groupClient } = this.bot.adapter.client
-        const call = promisify(groupClient.getGroupMemberList)
+        const call = promisify(groupClient.getGroupMemberList).bind(groupClient)
         return call({ groupId, refresh })
     }
 
     kickMember(groupId: number | string, targetUin: number | string, rejectAddRequest?: boolean, kickReason?: string) {
         const { groupClient } = this.bot.adapter.client
-        const call = promisify(groupClient.kickMember)
+        const call = promisify(groupClient.kickMember).bind(groupClient)
         return call({ groupId, targetUin, target: 'targetUin', rejectAddRequest, kickReason })
     }
 
     banMember(groupId: number | string, targetUin: number | string, duration: number) {
         // duration 单位：秒
         const { groupClient } = this.bot.adapter.client
-        const call = promisify(groupClient.banMember)
+        const call = promisify(groupClient.banMember).bind(groupClient)
         return call({ groupId, targetUin, target: 'targetUin', duration })
     }
 
     getFriendProfileCard(targetUins: (string | number)[]) {
         const { friendClient } = this.bot.adapter.client
-        const call = promisify(friendClient.getFriendProfileCard)
+        const call = promisify(friendClient.getFriendProfileCard).bind(friendClient)
         return call({ targetUins })
     }
 
     getFriendList(refresh?: boolean) {
         const { friendClient } = this.bot.adapter.client
-        const call = promisify(friendClient.getFriendList)
+        const call = promisify(friendClient.getFriendList).bind(friendClient)
         return call({ refresh })
     }
 
     getMessage(contact: Kritor.Contact, messageId: string) {
         const { messageClient } = this.bot.adapter.client
-        const call = promisify(messageClient.getMessage)
+        const call = promisify(messageClient.getMessage).bind(messageClient)
         return call({ contact, messageId })
     }
 
     // sham 报错 UNKNOWN
     getHistoryMessage(contact: Kritor.Contact, startMessageId?: string, count?: number) {
         const { messageClient } = this.bot.adapter.client
-        const call = promisify(messageClient.getHistoryMessage)
+        const call = promisify(messageClient.getHistoryMessage).bind(messageClient)
         return call({ contact, startMessageId, count })
+    }
+
+    setGroupWholeBan(groupId: number | string, isBan: boolean) {
+        const { groupClient } = this.bot.adapter.client
+        const call = promisify(groupClient.setGroupWholeBan).bind(groupClient)
+        return call({ groupId, isBan })
     }
 }
